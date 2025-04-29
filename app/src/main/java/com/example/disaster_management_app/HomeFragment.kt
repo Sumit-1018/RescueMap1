7724f9ed5b301d15d76b13acb62edf2d9f92d5db
Sumit Gupta <sumitgupta.1018@gmail.com>
2025-04-29 10:46:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/disaster_management_app/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.example.disaster_management_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.MapView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var mapView: MapView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        mapView = view.findViewById(R.id.mapView)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { googleMap ->
+            googleMap.uiSettings.isZoomControlsEnabled = true
+        }
+        return view
+    }
+
+    override fun onResume() { super.onResume(); mapView.onResume() }
+    override fun onPause() { super.onPause(); mapView.onPause() }
+    override fun onDestroy() { super.onDestroy(); mapView.onDestroy() }
+}
